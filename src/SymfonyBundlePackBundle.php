d278994fa8dc91e73eff6fc4f1aeed35ce61de2f
Asdingam <gloireasdingam@gmail.com>
2026-06-30 10:10:20 +0100

    fix: properly load services.yaml to register InstallCommand

--- src/SymfonyBundlePackBundle.php
+++ src/SymfonyBundlePackBundle.php
@@ -2,23 +2,21 @@
 
 namespace TonVendor\SymfonyBundlePack;
 
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-/**
- * Classe principale du bundle.
- * Enregistrée automatiquement dans config/bundles.php par Symfony Flex.
- */
 class SymfonyBundlePackBundle extends Bundle
 {
-    public function getContainerExtension(): ?ExtensionInterface
-    {
-        return null; // Pas de configuration propre, on délègue aux bundles tiers
-    }
-
     public function build(ContainerBuilder $container): void
     {
         parent::build($container);
+
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__ . '/../Resources/config')
+        );
+        $loader->load('services.yaml');
     }
 }
